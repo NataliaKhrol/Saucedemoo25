5ec5c79315f401c686d938cd03649a4b829492de
Natalia Khrol <natalia_khrol@epam.com>
2026-04-20 19:39:18 +0300

    BaseTest class is added

---
+++ src/test/java/pages/CartPage.java
@@ -0,0 +1,29 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartPage extends BasePage {
+    private final By product = By.cssSelector(".inventory_item_name");
+
+    public CartPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public ArrayList<String> getProductsNames() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("continue-shopping")));
+        List<WebElement> allProducts = driver.findElements(product);
+        ArrayList<String> names = new ArrayList<>();
+
+        for (WebElement product : allProducts) {
+            names.add(product.getText());
+        }
+
+        return names;
+    }
+}
---
+++ src/test/java/pages/NavigationPanel.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class NavigationPanel {
+    private final WebDriver driver;
+
+    private final By cartLink = By.cssSelector("[data-test='shopping-cart-link']");
+
+    public NavigationPanel(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void switchToCart() {
+        driver.findElement(cartLink).click();
+    }
+}
